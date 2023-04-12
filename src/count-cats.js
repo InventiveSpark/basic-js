251144d301de355415800148f125f885a08ba70e
InventiveSpark <ReadingDude@gmail.com>
2023-04-13 02:41:14 +0500

    feat: add 'count cats' solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  return matrix.reduce((counter, array) => (counter += array.reduce((arrayCounter, item) => (arrayCounter += (item === '^^')), 0)), 0);
 }
 
 module.exports = {
