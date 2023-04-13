783f8a77fca4ab75b0b9c05d760903ebd9fb2329
InventiveSpark <ReadingDude@gmail.com>
2023-04-13 09:25:08 +0500

    feat: add minesweeper solution

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+
+  const columnsNumber = matrix[0].length;
+  const rowsNumber = matrix.length;
+  const result = Array.from(Array(rowsNumber), () => new Array(columnsNumber)); // an empty two-dimensional array
+
+  // Get the sum of adjacent elements:
+  const adjSum = (row, col) => {
+    let sum = 0;
+    const minColNumber = Math.max((col - 1), 0);
+    const maxColNumber = Math.min((col + 2), columnsNumber);
+    const minRowNumber = Math.max((row - 1), 0);
+    const maxRowNumber = Math.min((row + 2), rowsNumber);
+    for (let i = minColNumber; i < maxColNumber; i++) { // cycle through columns
+      for (let j = minRowNumber; j < maxRowNumber; j++) { // cycle through rows
+        if (!(i === col && j === row)) // exclusion of the element itself
+          sum += matrix[j][i] ? 1 : 0;
+      }
+    }
+    return sum;
+  }
+
+  for (let i = 0; i < columnsNumber; i++) { // cycle through columns
+    for (let j = 0; j < rowsNumber; j++) { // cycle through rows
+      result[j][i] = adjSum(j, i);
+    }
+  }
+  return result;
 }
 
 module.exports = {
