9e7e97d07605933640843fa25f656b124ee880a8
InventiveSpark <ReadingDude@gmail.com>
2023-04-13 11:48:49 +0500

    feat: add 'carbon dating' solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,7 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-const MODERN_ACTIVITY = 15;
-const HALF_LIFE_PERIOD = 5730;
+const MODERN_ACTIVITY = 15; //  decay rate of carbon is 15 dpm/g 
+const HALF_LIFE_PERIOD = 5730; // The half-life for radioactive decay of 14C isotope is 5730 years
 
 /**
  * Determine the age of archeological find by using
@@ -17,9 +17,12 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if ((typeof sampleActivity === 'string') && // wrong type & no argument check
+    (0 < sampleActivity) && (sampleActivity < MODERN_ACTIVITY)) // validate parameter
+    return Math.ceil(HALF_LIFE_PERIOD * Math.log2(MODERN_ACTIVITY / sampleActivity));
+  return false;
+  // https://chem.libretexts.org/Bookshelves/Physical_and_Theoretical_Chemistry_Textbook_Maps/Supplemental_Modules_(Physical_and_Theoretical_Chemistry)/Nuclear_Chemistry/Nuclear_Kinetics/Half-Lives_and_Radioactive_Decay_Kinetics#mjx-eqn-21.4.2
 }
 
 module.exports = {
