3bbba459fc4677b1da459a51734ae7e24321ac58
InventiveSpark <ReadingDude@gmail.com>
2023-04-14 18:27:55 +0500

    feat: add 'Hanoi tower' solution

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,9 +14,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function calculateHanoi(disksNumber, turnsSpeed) {
+  // diskNumber is a number of disks
+  // turnsSpeed is the speed of moving discs (in turns per hour)
+
+  // For a tower of n disks, there will be required 2^n - 1 transfers
+  // of individual disks to shift the tower completely to another rod:
+  const turns = Math.pow(2, disksNumber) - 1;
+  const seconds = Math.floor(3600 * turns / turnsSpeed);
+
+  // Function returns an object with 2 properties:
+  return {
+    turns: turns, // minimum number of turns to solve the puzzle
+    seconds: seconds // minimum number of seconds to solve the puzzle at a given turnsSpeed
+  }
 }
 
 module.exports = {
